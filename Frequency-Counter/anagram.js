178087debba4055589e92d46cca4ce0d4a1fc16e
pratikghagare175 <pratikghagare1797@gmail.com>
2021-06-26 08:09:00 +0530

    anagram problem solving

--- Frequency-Counter/anagram.js
+++ Frequency-Counter/anagram.js
@@ -1,6 +1,7 @@
 // An Anagram is word, phrase or name formed by rearranging the letters of another
 // such as cinema, formed as iceman
 
+// ******************* MY SOLUTION **************************** //
 const validAnagram = (str1, str2) => {
   if (str1.length !== str2.length) return false;
 
@@ -27,3 +28,26 @@ const validAnagram = (str1, str2) => {
 };
 
 console.log(validAnagram("qwertyah", "qeywrtb "));
+
+// ********************** OPTIMIZED SOLUTION ****************** //
+
+const validAnagram = (str1, str2) => {
+  if (str1.length !== str2.length) return false;
+
+  const lookup = {};
+
+  for (const val of str1) {
+    lookup[val] ? (lookup[val] += 1) : (lookup[val] = 1);
+  }
+
+  for (const val of str2) {
+    if (!lookup[val]) {
+      return false;
+    } else {
+      lookup[val] -= 1;
+    }
+  }
+  return true;
+};
+
+console.log(validAnagram("qwerty", "ytrewh"));
